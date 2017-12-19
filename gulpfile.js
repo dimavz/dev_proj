a084572fe7ba5b6a686127e70094fd009ad0fbed
Dmitry <zatulenko@gmail.com>
2017-12-19 15:41:37 +0300

    Добавлена задача для перезагрузки браузера

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
     autoprefixer = require('gulp-autoprefixer'),
-    webserver = require('gulp-webserver');
+    browser = require('browser-sync');
 
 var config ={
     paths:{
@@ -17,32 +17,33 @@ var config ={
         pathCss:'src/css'
     },
     srv_options:{
-        basePath:'src/'
+        basePath:'src'
     }
 };
-
-gulp.task('webserver', function() {
-    gulp.src(config.srv_options.basePath)
-        .pipe(webserver({
-            livereload: true,
-            directoryListing: true,
-            open: true,
-            path: 'src/',
-            fallback: 'index.html'
-        }));
+gulp.task('browser', function(){
+    browser({
+        server: {
+            baseDir: config.srv_options.basePath
+        },
+        notify: false
+    });
 });
+
 
 gulp.task('scss', function () {
     gulp.src(config.paths.scss)
         .pipe(sass())
         .pipe(concat(config.output.nameFileCss))
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest(config.output.pathCss));
+        .pipe(gulp.dest(config.output.pathCss))
+        .pipe(browser.reload({stream: true}));
 });
 
-gulp.task('watch',['scss'], function () {
-    gulp.watch(config.paths.scss, ['scss', 'webserver']);
-    gulp.watch(config.paths.html,['webserver']);
+
+
+gulp.task('watch',['scss', 'browser'], function () {
+    gulp.watch(config.paths.scss, ['scss'],browser.reload);
+    gulp.watch(config.paths.html,browser.reload);
 });
 
 
